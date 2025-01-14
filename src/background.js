e14abc57e3ef2dac74c6589fa8624661bb3e845e
James McGahn <jmcgahn@gmail.com>
2025-01-14 14:51:57 -0500

    wrap storage endpoint call in promise, split into functions

--- src/background.js
+++ src/background.js
@@ -7,47 +7,61 @@ chrome.runtime.onInstalled.addListener((details) => {
 
   chrome.contextMenus.onClicked.addListener((event) => {
     if (event.menuItemId === "contextMenu1") {
-      let endpoint;
-      chrome.storage.sync.get(["endpoint"], (res) => {
-        endpoint = res.endpoint ?? "http://127.0.0.1:5000/word";
-      });
-      const url = endpoint;
-      const data = {
-        word: `${event.selectionText}`,
-      };
-
-      fetch(url, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      })
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error("Network response was not ok");
-          }
-          return response.json();
-        })
-        .then((data) => {
-          console.log("Success:", data);
-          this.registration.showNotification("EnglishDict Extension", {
-            body: `${data.word} was sent to EnglishDict Successfully`,
-            icon: "icon.png",
-          });
-        })
-        .catch((error) => {
-          if (
-            error instanceof TypeError &&
-            error?.message === "Failed to fetch"
-          ) {
-            this.registration.showNotification("EnglishDict Extension", {
-              body: `Failed to send ${data.word}. Make sure desktop application is opened and endpoint is correct.`,
-              icon: "icon.png",
-            });
-          }
-          console.log("Error:", error);
-        });
+      sendData(event);
     }
   });
 });
+
+function getEndpoint() {
+  return new Promise((resolve) => {
+    chrome.storage.sync.get(["endpoint"], (res) => {
+      resolve(res.endpoint ?? "http://127.0.0.1:5000/word");
+    });
+  });
+}
+
+async function sendData(event) {
+  const url = await getEndpoint();
+  const data = {
+    word: `${event.selectionText}`,
+  };
+
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        sendUserMessage(
+          `Failed to send ${data.word}. Make sure desktop application is opened and endpoint is correct.`,
+        );
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log("Success:", data);
+      this.registration.showNotification("EnglishDict Extension", {
+        body: `${data.word} was sent to EnglishDict Successfully`,
+        icon: "icon.png",
+      });
+    })
+    .catch((error) => {
+      if (error instanceof TypeError && error?.message === "Failed to fetch") {
+        sendUserMessage(
+          `Failed to send ${data.word}. Make sure desktop application is opened and endpoint is correct.`,
+        );
+      }
+      console.error("Error:", error);
+    });
+}
+
+function sendUserMessage(msg) {
+  this.registration.showNotification("EnglishDict Extension", {
+    body: msg,
+    icon: "icon.png",
+  });
+}
